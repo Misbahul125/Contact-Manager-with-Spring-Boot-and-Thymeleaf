a65793e7293767b93c9aaab460e8045b546ccef5
Misbahul Haque <misbahulhaque2001@gmail.com>
2022-10-11 12:50:27 +0530

    Spring security added

---
+++ contactmanager/src/main/java/com/contactmanager/config/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.contactmanager.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.contactmanager.models.User;
+import com.contactmanager.repos.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		//fetching user from database
+		User user = userRepository.getUserByUserName(username);
+		
+		if(user == null) {
+			throw new UsernameNotFoundException("Could not found user !!");
+		}
+		
+		CustomUserDetails customUserDetails = new CustomUserDetails(user);
+		
+		return customUserDetails;
+	}
+
+}
