cdc218007adb678124ab2fde793cf9d974245d9b
Misbahul Haque <misbahulhaque2001@gmail.com>
2022-10-12 21:54:13 +0530

    Add Contact UI added

--- contactmanager/src/main/java/com/contactmanager/controllers/UserController.java
+++ contactmanager/src/main/java/com/contactmanager/controllers/UserController.java
@@ -5,8 +5,11 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.contactmanager.models.Contact;
 import com.contactmanager.models.User;
 import com.contactmanager.repos.UserRepository;
 
@@ -16,16 +19,32 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
-
-	@RequestMapping("/index")
-	public String dashboard(Model model , Principal principal) {
+	
+	//to be automatically called beforea controller perform its action
+	@ModelAttribute
+	public void getUserData(Model model, Principal principal) {
 		
 		User user = userRepository.getUserByUserName(principal.getName());
 		
-		model.addAttribute("title", "Dashboard");
 		model.addAttribute("user", user);
 		
+	}
+
+	@GetMapping("/index")
+	public String dashboard(Model model) {
+		
+		model.addAttribute("title", "Dashboard");
+		
 		return "normal/user_dashboard";
+	}
+	
+	@GetMapping("/add-contact")
+	public String addContact(Model model) {
+		
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+		
+		return "normal/add-contact";
 	}
 	
 }
